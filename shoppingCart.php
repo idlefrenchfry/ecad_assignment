74cec3fcbe223956f06fcb4c3411b20171de210b
Tan Chin Kiat Damien <s10189272@connect.np.edu.sg>
2021-01-31 13:36:02 +0800

    Checkout update

--- shoppingCart.php
+++ shoppingCart.php
@@ -1,10 +1,10 @@
 <?php 
-// Include the code that contains shopping cart's functions
+
 include_once("cartFunctions.php");
 
-// Check if user logged in 
+
 if (! isset($_SESSION["ShopperID"])) {
-	// redirect to login page if the session variable shopperid is not set
+	
 	header ("Location: login.php");
 	exit;
 }
@@ -12,25 +12,23 @@ if (! isset($_SESSION["ShopperID"])) {
 $MainContent = "<div id='myShopCart' style='margin:auto'>";
 if (isset($_SESSION["Cart"])) {
 	include_once("mysql_conn.php");
-	// To Do 1 (Practical 4): 
-	// Retrieve from database and display shopping cart in a table
+	
 	$qry = "SELECT *, (Price*Quantity) AS Total
 		FROM ShopCartItem WHERE ShopCartID=?";
 	$stmt = $conn->prepare($qry) ;
-	$stmt->bind_param("i", $_SESSION["Cart"]); // “i" - integer
+	$stmt->bind_param("i", $_SESSION["Cart"]); 
 	$stmt->execute();
 	$result = $stmt->get_result();
 	$stmt->close();
 
-	// if redirected because delivery mode not selected when checking out
+
 	if (isset($_SESSION["deliveryModeNotSelected"])) {
 		echo $_SESSION["deliveryModeNotSelected"];
 		unset($_SESSION["deliveryModeNotSelected"]);
 	}
 	
 	if ($result->num_rows > 0) {
-		// To Do 2 (Practical 4): Format and display 
-		// the page header and header row of shopping cart page
+		
 		$MainContent .= "<p class='page-title' style='text-align:center'>Shopping Cart</p>"; 
 		$MainContent .= "<div class='table-responsive' >";
 		$MainContent .= "<table class='table table-hover'>"; 
@@ -43,13 +41,11 @@ if (isset($_SESSION["Cart"])) {
 		$MainContent .= "<th>&nbsp;</th>";
 		$MainContent .= "</tr>";
 		$MainContent .= "</thead>";
-		// To Do 5 (Practical 5):
-		// Declare an array to store the shopping cart items in session variable 
+		
 		$_SESSION["Items"]=array();
-		// Declare a variable to compute subtotal before tax
+		
 		$subTotal = 0; 
-		// To Do 3 (Practical 4):
-		// Display the shopping cart content
+	
 		$MainContent .= "<tbody>";
 		while ($row = $result->fetch_array()) {
 			$MainContent .= "<tr>";
@@ -58,15 +54,15 @@ if (isset($_SESSION["Cart"])) {
 			$formattedPrice = number_format($row["Price"], 2);
 			$MainContent .= "<td>$formattedPrice</td>";
 			$MainContent .= "<td>";
-			// Update quantity of purchase
+			
 			$MainContent .= "<form action='cartFunctions.php' method='post'>";
 			$MainContent .= "<select name='quantity' onChange='this.form.submit()'>";
-			for ($i = 1; $i <= 10; $i++) { // To populate drop-down list from 1 to 10
+			for ($i = 1; $i <= 10; $i++) { 
 				if($i == $row["Quantity"])
-					// Select the drop-down list item with value same as the quantity of purchase
+					
 					$selected = "selected";
 				else
-					$selected = ""; // No specific item is selected
+					$selected = ""; 
 				$MainContent .= "<option value='$i' $selected>$i</option>";
 			}
 			$MainContent .= "</select>";	
@@ -76,7 +72,7 @@ if (isset($_SESSION["Cart"])) {
 			$MainContent .= "</td>";
 			$formattedTotal = number_format($row["Total"], 2);
 			$MainContent .= "<td>$formattedTotal</td>";
-			// Remove Item
+			
 			$MainContent .= "<td>";
 			$MainContent .= "<form action='cartFunctions.php' method='post'>";
 			$MainContent .= "<input type='hidden' name='action' value='remove' />";
@@ -85,13 +81,12 @@ if (isset($_SESSION["Cart"])) {
 			$MainContent .= "</form>";
 			$MainContent .= "</td>";
 			$MainContent .= "</tr>";
-			// To Do 6 (Practical 5):
-		    // Store the shopping cart items in session variable as an associate array
+			
 			$_SESSION["Items"][]= array("productId"=>$row["ProductID"],
 										"name"=>$row["Name"],
 										"price"=>$row["Price"],
 										"quantity"=>$row["Quantity"]);
-			// Accumulate the running sub-total
+		
 			$subTotal += $row["Total"];
 		}
 		$MainContent .= "</tbody>";
@@ -117,12 +112,11 @@ if (isset($_SESSION["Cart"])) {
 		$MainContent .= "<label class='pl-2' onclick='this.form.submit()' for='express'>Express Delivery (Within 24 hours) $10</label><br>";
 		$MainContent .= "</form>";
 
-		// Display the subtotal at the end of the shopping cart
+		
 		$MainContent .= "<div><p style='text-align:right; font-size: 20px'>
 						Subtotal = S$". number_format($subTotal, 2) . "</p>";
 		$_SESSION["SubTotal"] = round($subTotal, 2);
-		// To Do 7 (Practical 5):
-		// Add PayPal Checkout button on the shopping cart page
+		
 		$MainContent .= "<form method='post' action='checkoutProcess.php'>";
 		$MainContent .= "<input type='image'
 						src='https://www.paypal.com/en_US/i/btn/btn_xpressCheckout.gif'>";
@@ -133,7 +127,7 @@ if (isset($_SESSION["Cart"])) {
 	else {
 		$MainContent .= "<h3 style='text-align:center; color:red;'>Empty shopping cart!</h3>";
 	}
-	$conn->close(); // Close database connection
+	$conn->close(); 
 }
 else {
 	$MainContent .= "<h3 style='text-align:center; color:red;'>Empty shopping cart!</h3>";
@@ -150,7 +144,6 @@ if (isset($_POST["delivery"])) {
 	
 	$MainContent = "";
 
-	// refresh page and update delivery
 	echo "<meta http-equiv='refresh' content='0'>";
 }
 
