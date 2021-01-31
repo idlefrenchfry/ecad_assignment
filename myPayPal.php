2615dfa000b8bfb6abf26772167f78b6ceed2e91
Ana <anakatrina1101@gmail.com>
2021-01-31 18:31:22 +0800

    resolved previous merge

--- myPayPal.php
+++ myPayPal.php
@@ -1,12 +1,5 @@
 <?php
-<<<<<<< HEAD
-$PayPalMode 		= 'sandbox';
-$PayPalApiUsername 	= 'sb-xlezv3951511_api1.business.example.com'; 		
-$PayPalApiPassword 	= 'NTLTWTC9FN7FP3CH'; 		
-$PayPalApiSignature = 'At7M.vjpvoOSoEuUejMvbAJc494kAAhCqtUD2Y1sXe80Ctc5jb-1XQlQ'; 		
-$PayPalCurrencyCode = 'SGD'; 	
-$PayPalReturnURL 	= 'http://localhost:8081/ecad_assignment/checkoutProcess.php';               
-=======
+
 $PayPalMode 		= 'sandbox';
 $PayPalApiUsername 	= 'sb-xlezv3951511_api1.business.example.com'; 		
 $PayPalApiPassword 	= 'NTLTWTC9FN7FP3CH'; 		
@@ -14,8 +7,7 @@ $PayPalApiSignature = 'At7M.vjpvoOSoEuUejMvbAJc494kAAhCqtUD2Y1sXe80Ctc5jb-1XQlQ'
 $PayPalCurrencyCode = 'SGD'; 	
 
 $PayPalReturnURL 	= 'http://localhost:8081/ecad_assignment/checkoutProcess.php'; 
-                
->>>>>>> origin/main
+
 $PayPalCancelURL 	= 'http://localhost:8081/ecad_assignment/shoppingcart.php'; 
                 	
 function PPHttpPost($methodName_, $nvpStr_, $PayPalApiUsername, $PayPalApiPassword, 
